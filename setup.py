f3e25b07af0728ae07c9f419ba4e7238b0ecac6b
Jacob Huth <jahuth@uos.de>
2017-11-30 11:26:15 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'convis',
   packages = ['convis','convis.filters'],
-  version = '0.5',
+  version = '0.5.0.2',
   install_requires = ["matplotlib", "litus", "numpy", "future"],
   description = 'Convolutional Vision Model',
   author = 'Jacob Huth',
   author_email = 'jahuth@uos.de',
   url = 'https://github.com/jahuth/convis',
-  download_url = 'https://github.com/jahuth/convis/tarball/0.5',
+  download_url = 'https://github.com/jahuth/convis/tarball/0.5.0.2',
   keywords = ['vision model', 'retina model'],
   classifiers = [],
 )
