f2b15c4c1107b89365dcd23935b6756ee1c848dc
Jacob Huth <jahuth@uos.de>
2018-03-07 13:45:14 +0100

    Tagging Version 0.5.2.1

--- convis/__init__.py
+++ convis/__init__.py
@@ -5,7 +5,7 @@ except ImportError:
     print('Please download the torch package from https://pytorch.org')
     raise
 
-__version__ = '0.5.2.0'
+__version__ = '0.5.2.1'
 
 from . import base
 from . import retina
